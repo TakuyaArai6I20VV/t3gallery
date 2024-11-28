83ff5986e78ba67bf7de3a3ecf214b52d7c2766b
TakuyaArai6I20VV <leetgrow@gmail.com>
2024-11-28 22:30:39 +0900

    scaffold UI

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 ## TODO
 
 - [✔️] Make it deploy (vercel)
-- [ ] Scaffold basic ui with mock data
+- [✔️] Scaffold basic ui with mock data
 - [ ] Actually set up (vercel postgres)
 - [ ] Attach database to UI
 - [ ] Add authentication (w/ clerk)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,27 @@
 import Link from "next/link";
 
+const mockUrls = [
+  "https://utfs.io/f/XkUJIT9h5yjKIMxTwokkG9jhmEzuQXr4vnxdsBbNUa6StVel",
+  "https://utfs.io/f/XkUJIT9h5yjKEnMvHOUtlrBRVhGubFg35TjU1AkNaw0QWKHC",
+  "https://utfs.io/f/XkUJIT9h5yjKEnMvHOUtlrBRVhGubFg35TjU1AkNaw0QWKHC",
+  "https://utfs.io/f/XkUJIT9h5yjK5IhEetWXUnHSwNzLx8WkYyD7Jcamu5P6RoEe",
+]
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}))
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Hello gallery
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {[...mockImages, ...mockImages, ...mockImages].map((image) => (
+          <div key={image.id} className="w-48">
+            <img src={image.url} alt={`firebase icon ${image.id}`} />
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
